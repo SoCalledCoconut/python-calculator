31223c515d3a69ee39d02fe4ca6f6a7e80cef26f
Shravan <69118069+Shravan-1908@users.noreply.github.com>
2020-10-01 13:50:03 +0530

    Update python_calculator.py
    Added calorie calculation and removed case sensitiveness from all of your code.

--- python_calculator.py
+++ python_calculator.py
@@ -1,31 +1,29 @@
-
-
-# defining functions and lists for BASIC choice
 user_functions = ["Add", "Multiply", "Divide", "Subtract"]
 
 def basic_input():
     num_prompt = float(input("Please enter a number: "))
     num_prompt_2 = float(input("Please enter another number: "))
     user_function = input("What woud you like to do? (Add/Multiply/Divide/Subtract) ")
-    if user_function in user_functions:
+    if user_function.capitalize() in user_functions:
         basic_calculation(user_function, num_prompt, num_prompt_2)
     else:
         print("I'm sorry, you've entered something incorrectly - try again!")
         return basic_input()
 
 def basic_calculation(user_function, num_prompt, num_prompt_2):
-        if user_function == "Add":
-            addition = float(num_prompt + num_prompt_2)
-            print("The result of these two numbers added together is", addition)
-        if user_function == "Multiply":
-            multiply = float(num_prompt  * num_prompt_2)
-            print("The result of these two numbers multiplied is ", multiply)
-        if user_function == "Divide":
-            divide = float(num_prompt / num_prompt_2)
-            print("The result of these two numbers divided is", divide)
-        if user_function == "Subtract":
-            subtract = float(num_prompt - num_prompt_2)
-            print("The result of these two numbers divided is", subtract)
+    user_function = user_function.capitalize()
+    if user_function == "Add":
+        addition = float(num_prompt + num_prompt_2)
+        print("The result of these two numbers added together is", addition)
+    if user_function == "Multiply":
+        multiply = float(num_prompt  * num_prompt_2)
+        print("The result of these two numbers multiplied is ", multiply)
+    if user_function == "Divide":
+        divide = float(num_prompt / num_prompt_2)
+        print("The result of these two numbers divided is", divide)
+    if user_function == "Subtract":
+        subtract = float(num_prompt - num_prompt_2)
+        print("The result of these two numbers substracted is", subtract)
         
 #defining functions and lists for ADVANCED choice
 adv_functions = ["Power", "Square Root"]
@@ -35,17 +33,17 @@ def adv_input():
     num_prompt = float(input("Please enter a number: "))
     num_prompt_2 = float(input("Please enter another number:\n(if you want to use the square root funtion, please enter '0'.)"))
     adv_function = input("What woud you like to do? (Power/Square Root) ")
-    if adv_function in adv_functions:
+    if adv_function.capitalize() in adv_functions:
         adv_calculation(adv_function, num_prompt, num_prompt_2)
     else:
         print("I'm sorry, you've entered something incorrectly - try again!")
         return adv_input()
 
 def adv_calculation(adv_function, num_prompt, num_prompt_2):
-        if adv_function == "Power":
+        if adv_function.capitalize() == "Power":
             power = float(pow(num_prompt, num_prompt_2))
             print("The result of the first number to the power of the second number is", power)
-        if adv_function == "Square Root":
+        if adv_function.capitalize() == "Square Root":
             square = float(math.sqrt(num_prompt))
             print("The square root of ", num_prompt, " is ", square)
 
@@ -62,7 +60,7 @@ def bmi_met():
     elif bmi_met_res <= 30:
         print("Your BMI is ", bmi_met_res, " which is considered overweight.")
     elif bmi_met_res >= 30.1:
-        print("Your BMI is ", bmi_met_res, " which is considered obese.")  
+        print("Your BMI is ", bmi_met_res, " which is considered obese.") 
 
 def bmi_imp():
     height_imp = 2.205*float(input("Please enter your height in feet (use decimals): " ))
@@ -77,6 +75,33 @@ def bmi_imp():
         print("Your BMI is ", bmi_imp_res, " which is considered overweight.")
     elif bmi_imp_res >= 30.1:
         print("Your BMI is ", bmi_imp_res, " which is considered obese.")  
+
+
+# CALCULATING CALORIES - added only for imperial units
+def calories(): 
+    weight = float(input("Enter your weight(in kgs): "))
+    height = float(input("Enter your height(in metres): "))
+    age = int(input("Enter your age: "))
+    gender = input("Enter your gender(M/F): ")
+
+    # BEE stands for basal energy expenditure
+    BEEm = 66.5 + (13.75 * weight) + (5.003 * height*100) - (6.775 * age) #for males
+    BEEf = 655.1 + (9.563 * weight) + (1.850 * height*100) - (4.676 * age) #for females
+    actlvl =  int(input("What is your activity level:\n1. Little or no exercise\n2. Light exercise/sports\n3. Medium exercise/sports\n4. Hard exercise/sports\n5. Intense exercise/sports\n"))
+
+    actlvl_ratios = {1:1.2, 2:1.375, 3:1.55, 4:1.1725, 5:1.9} # a dict containing ratios to multiply with the respective BEE for calculating calories
+
+    if gender.lower() == 'm':
+        calorie_intake = BEEm * actlvl_ratios.get(actlvl)
+        print(f"Your daily required calories is {calorie_intake}")
+    elif gender.lower() == 'f':
+        calorie_intake = BEEf * actlvl_ratios.get(actlvl)
+        print(f"Your daily required calories is {calorie_intake}")
+    else:
+        print("Error: Invalid inputs!")
+    
+
+
 
 #defining functions for Mortgage calculation
 def mort_calc():
@@ -113,28 +138,34 @@ def trip_calc():
 #Calculator Selection
 
 def begin():
-    print("WELCOME TO THE PYTHON CALCULATOR\n\nYOUR SELECTIONS:\nBasic (+, -, *, /)\nAdvanced (power, square root)\nBMI\nMortgage\nTrip\n")
-    selection = input("What type of Calculator would you like to use? (NB: Case sensitive): ")
-
+    print("WELCOME TO THE PYTHON CALCULATOR\n\nYOUR SELECTIONS:\nBasic (+, -, *, /)\nAdvanced (power, square root)\nBMI\nCalories\nMortgage\nTrip\n")
+    selection = input("What type of Calculator would you like to use?: ")
+    selection = selection.capitalize()
     if selection == "Basic":
         basic_input()
-    if selection == "Advanced":
+    elif selection == "Advanced":
         adv_input()
-    if selection == "BMI":
+    elif selection.upper() == "BMI":
         imp_or_met = input("Do you prefer to use metric or imperial units? (Metric/Imperial): ")
-        if imp_or_met == "Metric":
+        if imp_or_met.capitalize() == "Metric":
             bmi_met()
-        elif imp_or_met == "Imperial":
+        elif imp_or_met.capitalize() == "Imperial":
             bmi_imp()
-    if selection == "Mortgage":
+    elif selection == "Calories":
+        calories()
+    elif selection == "Mortgage":
         mort_calc()
-    if selection == "Trip":
+    elif selection == "Trip":
         trip_calc()
+    else:
+        print("Invalid input!")
 
-begin()
-#restart calculator
-restart = input("Would you like to use the calculator again?\n1: Yes, 2: No\n")
-if restart == "1":
+
+if __name__ == "__main__":
     begin()
-else:
-    print("Thank you for using the calculator!")
+    #restart calculator
+    restart = input("Would you like to use the calculator again?\n1: Yes\n2: No\n")
+    if restart == "1":
+        begin()
+    else:
+        print("Thank you for using the calculator!")
